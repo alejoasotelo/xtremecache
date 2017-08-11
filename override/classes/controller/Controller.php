7a409a01e9a13850ce1a3604fb1437623b1525b4
Alejo A. Sotelo <alejoasotelo@outlook.com>
2017-08-11 11:44:31 -0300

    Add compatibility with prestashop 1.5

--- override/classes/controller/Controller.php
+++ override/classes/controller/Controller.php
@@ -12,64 +12,75 @@ abstract class Controller extends ControllerCore
     {
         $this->context->cookie->write();
 
-        $html = '';
-        $js_tag = 'js_def';
-        $this->context->smarty->assign($js_tag, $js_tag);
+        if (_PS_VERSION_ < 1.6) {
 
-        if (is_array($content)) {
-            foreach ($content as $tpl) {
-                $html = $this->context->smarty->fetch($tpl);
-            }
-        } else {
-            $html = $this->context->smarty->fetch($content);
-        }
-
-        $html = trim($html);
-
-        if (in_array($this->controller_type, array('front', 'modulefront')) && !empty($html) && $this->getLayout()) {
-            $live_edit_content = '';
-            if (!$this->useMobileTheme() && $this->checkLiveEditAccess()) {
-                $live_edit_content = $this->getLiveEditFooter();
-            }
-
-            $dom_available = extension_loaded('dom') ? true : false;
-            $defer = (bool)Configuration::get('PS_JS_DEFER');
-
-            if ($defer && $dom_available) {
-                $html = Media::deferInlineScripts($html);
-            }
-            $html = trim(str_replace(array('</body>', '</html>'), '', $html))."\n";
-
-            $this->context->smarty->assign(array(
-                $js_tag => Media::getJsDef(),
-                'js_files' => array_unique($this->js_files),
-                'js_inline' => $dom_available ? Media::getInlineScript() : array()
+            Hook::exec('actionRequestComplete', array(
+                'controller' => $this,
+                'output' => $content
             ));
+            $this->context->smarty->display($content);
+        } else {
 
-            $javascript = $this->context->smarty->fetch(_PS_ALL_THEMES_DIR_.'javascript.tpl');
+            $html = '';
+            $js_tag = 'js_def';
+            $this->context->smarty->assign($js_tag, $js_tag);
 
-            $output = '';
-            if ($defer && (!isset($this->ajax) || ! $this->ajax)) {
-                $output = $html.$javascript;
+            if (is_array($content)) {
+                foreach ($content as $tpl) {
+                    $html = $this->context->smarty->fetch($tpl);
+                }
             } else {
-                $output = preg_replace('/(?<!\$)'.$js_tag.'/', $javascript, $html);
+                $html = $this->context->smarty->fetch($content);
             }
 
-            $output .= $live_edit_content.((!isset($this->ajax) || ! $this->ajax) ? '</body></html>' : '');
+            $html = trim($html);
 
-            Hook::exec('actionRequestComplete', array(
-                'controller' => $this,
-                'output' => $output
-            ));
+            if (in_array($this->controller_type, array('front', 'modulefront')) && !empty($html) && $this->getLayout()) {
+                $live_edit_content = '';
+                if (!$this->useMobileTheme() && $this->checkLiveEditAccess()) {
+                    $live_edit_content = $this->getLiveEditFooter();
+                }
 
-            echo $output;
-        } else {
-            Hook::exec('actionRequestComplete', array(
-                'controller' => $this,
-                'output' => $html
-            ));
+                $dom_available = extension_loaded('dom') ? true : false;
+                $defer = (bool)Configuration::get('PS_JS_DEFER');
 
-            echo $html;
+                if ($defer && $dom_available) {
+                    $html = Media::deferInlineScripts($html);
+                }
+                $html = trim(str_replace(array('</body>', '</html>'), '', $html))."\n";
+
+                $this->context->smarty->assign(array(
+                    $js_tag => Media::getJsDef(),
+                    'js_files' => array_unique($this->js_files),
+                    'js_inline' => $dom_available ? Media::getInlineScript() : array()
+                ));
+
+                $javascript = $this->context->smarty->fetch(_PS_ALL_THEMES_DIR_.'javascript.tpl');
+
+                $output = '';
+                if ($defer && (!isset($this->ajax) || ! $this->ajax)) {
+                    $output = $html.$javascript;
+                } else {
+                    $output = preg_replace('/(?<!\$)'.$js_tag.'/', $javascript, $html);
+                }
+
+                $output .= $live_edit_content.((!isset($this->ajax) || ! $this->ajax) ? '</body></html>' : '');
+
+                Hook::exec('actionRequestComplete', array(
+                    'controller' => $this,
+                    'output' => $output
+                ));
+
+                echo $output;
+            } else {
+                Hook::exec('actionRequestComplete', array(
+                    'controller' => $this,
+                    'output' => $html
+                ));
+
+                echo $html;
+            }
+
         }
     }
 }
